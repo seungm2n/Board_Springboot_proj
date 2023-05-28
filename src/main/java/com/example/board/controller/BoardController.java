b7586545113115fa59ee633c26bb1484f401bdde
seungm2n <seungm2n2@gmail.com>
2023-05-29 04:03:32 +0900

    Initial commit

---
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -0,0 +1,36 @@
+package com.example.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+// HTTP요청을 받아 응답하는 컴포넌트. 스프링부트가 자동으로 Bean 생성함
+@Controller
+public class BoardController {
+    // 게시물 목록을 보여줌
+    // 컨트롤러의 메소드가 리턴하는 문자열은 템플릿
+    // http://localhost:8080/ ===> "list"라는 이름의 템플릿을 사용하여 화면에 출력.(forwarding)
+    // list를 리턴한다는 것은
+    // classpath:/templates/list.html
+    @GetMapping("/")
+    public String list() {
+        // 게시물 목록 읽어오고, 페이징 처리
+        return "list";
+    }
+
+    // /board?id=3 : 파라미터 id,  파마미터 id의 값은 3
+    // /board?id=2
+    // /board?id=1
+    @GetMapping("/board")
+    public String board(@RequestParam("id") int id) {
+        System.out.println("id : " + id);
+
+        // id에 해당하는 게시물을 읽어옴
+        // id에 해당하는 게시물의 조회수도 1 증가
+        
+        return "board";
+    }
+
+    // 삭제. 관리자는 모든 글 삭제 가능
+    // 수정.
+}
---
+++ src/main/java/com/example/board/controller/UserController.java
@@ -0,0 +1,59 @@
+package com.example.board.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class UserController {
+    // http://localhost:8080/userRegForm
+    // classpath:/templates/userRegForm.html
+    @GetMapping("/userRegForm")
+    public String UserRegForm() {
+        return "userRegForm";
+    }
+
+    @PostMapping("/userReg")
+    public String UserReg(
+            @RequestParam("name") String name,
+            @RequestParam("email") String email,
+            @RequestParam("password") String password
+    ) {
+        System.out.println("name : " + name);
+        System.out.println("email : " + email);
+        System.out.println("password : " + password);
+        
+        // 회원 정보를 저장
+        
+        return "redirect:/welcome"; // 브라우저에게 자동으로 http://localhost:8080/welcome으로 이동
+    }
+
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "welcome";
+    }
+
+    @GetMapping("/loginform")
+    public String loginform() {
+        return "loginform";
+    }
+
+    @PostMapping("/login")
+    public String login(
+            @RequestParam("email") String email,
+            @RequestParam("password") String password
+    ) {
+        // email에 해당하는 회원 정보를 읽어 온 후
+        // 아이디 암호가 맞다면 세션에 회원정보를 저장
+        System.out.println("email : " + email);
+        System.out.println("password : " + password);
+        return "redirect:/";
+    }
+
+    @GetMapping("/logout")
+    public String logout() {
+        // 세션에서 회원정보 제거
+        return "redirect:/";
+    }
+}
